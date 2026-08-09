dda523f54e6faa9a531aba1603210331542147d7
Sanjeet Ambavale <sa1ambavale@gmail.com>
2026-08-09 17:30:21 +0530

    Obscure database user and password
    Updated database connection credentials to use placeholders.

--- database.py
+++ database.py
@@ -2,8 +2,8 @@ import mysql.connector
 
 connection = mysql.connector.connect(
     host="localhost",
-    user="root",
-    password="root",
+    user="*****",
+    password="******",
     database="smart_inventory"
 )
 
